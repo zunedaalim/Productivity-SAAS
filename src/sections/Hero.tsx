2ab95ff0b3d1cabb8f5dd91f46b5285b9b06a21f
Zuned Aalim <zunedaalim@gmail.com>
2025-02-15 01:27:27 +0530

    feat: Add hero section with headline, subheading, and CTA

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,8 +1,35 @@
+import ArrowIcon from "@/assets/arrow-right.svg";
+import cogImage from "@/assets/cog.png";
+import Image from "next/image";
 export const Hero = () => {
   return (
-    <section>
+    <section className=" pt-8 pb-20 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_66%)]">
       <div className="container">
-        <h1>What is this?</h1>
+        <div className="md:flex items-center justify-between">
+          <div>
+            <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">
+              Version 2.0 is Here
+            </div>
+            <h1 className="text-5xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5">
+              Pathway to productivity
+            </h1>
+            <p className="text-xl text-[#010D3E] tracking-tight mt-6 ">
+              Celebrate the joy of accomplishment with an app designed to track
+              your progress, motivate your efforts, and celebrate your
+              successes.
+            </p>
+            <div className="flex gap-1 items-center mt-[30px] ">
+              <button className="btn btn-primary">Get for free</button>
+              <button className="btn btn-text gap-1">
+                <span>Learn more</span>
+                <ArrowIcon className="h-5 w-5 " />
+              </button>
+            </div>
+          </div>
+          <div className="mt-20">
+            <Image src={cogImage} alt="cog" width={500} height={500}></Image>
+          </div>
+        </div>
       </div>
     </section>
   );
