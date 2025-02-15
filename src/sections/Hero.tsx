3c0c663f3eb6de679d2197c41062b81b6b9e4ba7
Zuned Aalim <zunedaalim@gmail.com>
2025-02-15 11:20:50 +0530

    feat: Finalize hero section implementation and styling

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,16 +1,18 @@
 import ArrowIcon from "@/assets/arrow-right.svg";
 import cogImage from "@/assets/cog.png";
+import cylinderImage from "@/assets/cylinder.png";
+import noodleImage from "@/assets/noodle.png";
 import Image from "next/image";
 export const Hero = () => {
   return (
-    <section className=" pt-8 pb-20 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_66%)]">
+    <section className=" pt-8 pb-20 md:pt-5 md:pb-10  bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip">
       <div className="container">
         <div className="md:flex items-center justify-between">
-          <div>
+          <div className="md:w-[478px]">
             <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">
               Version 2.0 is Here
             </div>
-            <h1 className="text-5xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5">
+            <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5">
               Pathway to productivity
             </h1>
             <p className="text-xl text-[#010D3E] tracking-tight mt-6 ">
@@ -26,8 +28,26 @@ export const Hero = () => {
               </button>
             </div>
           </div>
-          <div className="mt-20">
-            <Image src={cogImage} alt="cog" width={500} height={500}></Image>
+          <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
+            <Image
+              src={cogImage}
+              alt="cog"
+              width={500}
+              height={500}
+              className="md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0"
+            ></Image>
+            <Image
+              src={cylinderImage}
+              alt={"Cylinder Image"}
+              width={220}
+              className="hidden md:block -top-8 -left-32 md:absolute"
+            ></Image>
+            <Image
+              src={noodleImage}
+              alt="Noodle Image"
+              width={220}
+              className=" hidden lg:block rotate-[30deg] absolute top-[524px] left-[448px]"
+            ></Image>
           </div>
         </div>
       </div>
